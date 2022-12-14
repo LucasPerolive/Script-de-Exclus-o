70414e8792f5e8024c448fb41260a75339a9b817
Lucas Perdigão De Oliveira <116082415+LucasPerolive@users.noreply.github.com>
2022-12-14 08:57:30 -0300

    Update Script-exclusão.ps1

--- Script-exclusão.ps1
+++ Script-exclusão.ps1
@@ -1,3 +1,4 @@
+#Define um hashtable, uma estrutura de dados para mapear chaves exclusivas para valores
 $nameExpr = @{
 	Label="Feito";
 	Expression = {(del $_.Name)}
@@ -5,6 +6,7 @@ $nameExpr = @{
 
 echo "Caso não queira especificar a data, hora ou extensao de arquivo digite (*)"
 
+#Definir a data e hora
 echo "Digite a data no formato MM/DD/AAAA com as '/'"
 $dia = Read-Host 
 echo "A data esta correta $dia ? S ou N:"
@@ -15,25 +17,32 @@ $hora = Read-Host
 echo "A hora esta correta $hora ? S ou N:"
 $horario = Read-Host
 
+#Confere se a data  e hora estao corretas
+if ($opcao -eq "S" -or $opcao -eq "s" -and $horario -eq "S" -or $horario -eq "s") {
 
-if ($opcao -eq "S" -or $opcao -eq "s") {
-
-
+# Definir o diretorio e tipo de arquivo
 $diretorio = Read-Host "Diretorio desejado"
 $tipo = Read-Host "Se for uma pasta digite P		Se for um arquivo digite A"
 
+
+#Para arquivos
 if($tipo -eq "A" -or $tipo -eq "a"){
 
+#Definir a extensao do arquivo
 $extensao = Read-Host "Extensao do arquivo"
+#Filtra para ser apenas a extensao desejada
 Get-childItem $diretorio -Filter "*$extensao" -Recurse |
+#Proucra pela data e hora
 Where-Object LastWriteTime -like "$dia *$hora*" |
+#Exclui os objetos
 Select-Object $nameExpr
 echo "FEITO."
 } 
 
-
+#Para Diretorios
 if($tipo -eq "P" -or $tipo -eq "p"){
 
+#A diferenaca deste para o de arquivos e que ele passa por duas pesquisa de criterios um para dia e hora e outra para ver se ele é um diretorio
 Get-childItem $diretorio -Recurse |
 Where-Object LastWriteTime -like "$dia *$hora*" |
 Select-Object Name ,mode|
